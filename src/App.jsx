e6758fe92664a1c61062fb476fa404d323809230
Rohan S Mirjankar <rohansm668@gmail.com>
2026-01-26 00:01:35 +0530

    Update App.jsx
    change text heading

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <div className="Container">
-        <h1>Under Construction</h1>
+        <h1>Site Under Construction</h1>
         <img src={construction} alt="Under Construction" className="construction-gif" loading="eager" style={{ aspectRatio: '1 / 1' }}/>
         <h1>We are creating something cool ⚙️</h1>
       </div>
