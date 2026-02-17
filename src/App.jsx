b06039273f0fc78726de59bfb5afa8d24792cdd4
Rohan-Shridhar <rohansm668@gmail.com>
2026-02-17 18:33:36 +0530

    added grids to containers

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,13 @@ function App() {
   return (
     <>
       <div className="Container">
-        <h1>Site Under Construction</h1>
-        <img src={construction} alt="Under Construction" className="construction-gif" loading="eager" style={{ aspectRatio: '1 / 1' }}/>
-        <h1>We are creating something cool ⚙️</h1>
+        <div></div>
+        <div>
+          <h1>Site Under Construction</h1>
+          <img src={construction} alt="Under Construction" className="construction-gif" loading="eager" style={{ aspectRatio: '1 / 1' }} />
+          <h1>We are creating something cool ⚙️</h1>
+        </div>
+        <div></div>
       </div>
     </>
   );
